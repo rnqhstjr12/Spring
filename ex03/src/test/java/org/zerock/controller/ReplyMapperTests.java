cdad9a46385298853a0d6092b8fc05a7149e6d97
rnqhstjr12 <rnqhstjr12@gmail.com>
2023-11-07 17:58:05 +0900

    9

---
+++ ex03/src/test/java/org/zerock/controller/ReplyMapperTests.java
@@ -0,0 +1,42 @@
+package org.zerock.controller;
+
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.ReplyVO;
+import org.zerock.mapper.ReplyMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ReplyMapperTests  {
+	private Long[] bnoArr = { 3145745L, 3145744L, 3145743L, 3145742L, 3145741L };
+	
+	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+	
+	@Test
+	public void testMapper() {
+		log.info(mapper);
+	}
+	
+	@Test
+	public void testCreate() {
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			ReplyVO vo = new ReplyVO();
+			
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글 테스트 " + i);
+			vo.setReplyer("replyer" + i);
+			
+			mapper.insert(vo);
+		});
+	}
+}
